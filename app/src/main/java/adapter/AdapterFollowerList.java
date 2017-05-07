75eae3f78fd6866f241b8f74a3bd52aefcabf305
KienBui1995 <kien.laohac@gmail.com>
2017-05-08 02:33:18 +0700

    add Follower

---
+++ app/src/main/java/adapter/AdapterFollowerList.java
@@ -0,0 +1,227 @@
+package adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Theme;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.joker.hoclazada.R;
+import com.joker.hoclazada.Ultil.VolleyHelper;
+import com.joker.hoclazada.UserProfileActivity;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import Entity.EntityFollow;
+import Entity.EntityUserProfile;
+import io.realm.Realm;
+
+/**
+ * Created by joker on 4/25/17.
+ */
+
+public class AdapterFollowerList extends ArrayAdapter {
+    //param
+    Activity context;
+    int resource;
+    ArrayList<EntityFollow> items;
+    //view
+    private ImageView imgAvatarFollow;
+    private TextView txtFullNameFollow;
+    private TextView txtUserNameFollow;
+    private Button btnMessage;
+
+    public AdapterFollowerList(Activity context, int resource, ArrayList objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.resource = resource;
+        this.items = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = this.context.getLayoutInflater();
+        View view = inflater.inflate(this.resource,null);
+        addControl(view);
+        addEvent(position);
+        return view;
+    }
+
+    private void addEvent(final int position) {
+        imgAvatarFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, UserProfileActivity.class);
+                intent.putExtra("uId",items.get(position).getId());
+                context.startActivity(intent);
+            }
+        });
+        txtFullNameFollow.setText(items.get(position).getFull_name());
+        txtFullNameFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, UserProfileActivity.class);
+                intent.putExtra("uId",items.get(position).getId());
+                context.startActivity(intent);
+            }
+        });
+        txtUserNameFollow.setText("@"+items.get(position).getUsername());
+
+        if (items.get(position).is_followed()) {
+            btnMessage.setBackgroundResource(R.drawable.btn_unfollow);
+            btnMessage.setTextColor(Color.parseColor("#FFFFFF"));
+            btnMessage.setText("Đã theo dõi");
+            btnMessage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new MaterialDialog.Builder(context)
+                            .content("Bạn có muốn bỏ theo dõi " + items.get(position).getFull_name() + " ?")
+                            .positiveText(R.string.agree)
+                            .theme(Theme.LIGHT)
+                            .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                @Override
+                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+                                    Log.d("positionId", items.get(position).getId());
+                                    unFollow(Integer.parseInt(items.get(position).getId()));
+                                    btnMessage.setText("Theo dõi");
+                                    notifyDataSetChanged();
+                                }
+                            })
+                            .negativeText(R.string.disagree)
+                            .show();
+
+
+                }
+            });
+        } else {
+            btnMessage.setText("Theo dõi");
+            btnMessage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new MaterialDialog.Builder(context)
+                            .content("Bạn có muốn theo dõi " + items.get(position).getFull_name() + " ?")
+                            .positiveText(R.string.agree)
+                            .theme(Theme.LIGHT)
+                            .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                @Override
+                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                    Log.d("positionId", items.get(position).getId());
+                                    btnMessage.setBackgroundResource(R.drawable.btn_unfollow);
+                                    btnMessage.setTextColor(Color.parseColor("#FFFFFF"));
+                                    btnMessage.setText("Đã theo dõi");
+                                    btnMessage.setEnabled(false);
+                                    Follow(Integer.parseInt(items.get(position).getId()));
+                                    Intent intent = new Intent(context, UserProfileActivity.class);
+                                    intent.putExtra("uId", items.get(position).getId());
+                                    context.startActivity(intent);
+                                }
+                            })
+                            .negativeText(R.string.disagree)
+                            .show();
+
+                }
+            });
+
+
+        }
+
+
+//        //sss
+//        btnMessage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                new MaterialDialog.Builder(context)
+//                        .content("Bạn có muốn theo dõi "+items.get(position).getFull_name()+ " ?")
+//                        .positiveText(R.string.agree)
+//                        .theme(Theme.LIGHT)
+//                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+//                            @Override
+//                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+//
+//                                Log.d("positionId",items.get(position).getId());
+//                                Follow(Integer.parseInt(items.get(position).getId()));
+//                                items.remove(position);
+//                                notifyDataSetChanged();
+//                            }
+//                        })
+//                        .negativeText(R.string.disagree)
+//                        .show();
+//
+//
+//            }
+//        });
+//        //sssssss
+
+
+
+
+    }
+
+
+    private void addControl(View view) {
+        imgAvatarFollow = (ImageView) view.findViewById(R.id.imgAvatarFollow);
+        txtFullNameFollow = (TextView) view.findViewById(R.id.txtFullNameFollow);
+        txtUserNameFollow = (TextView) view.findViewById(R.id.txtUserNameFollow);
+        btnMessage = (Button) view.findViewById(R.id.btnMessage);
+    }
+
+    private void Follow(int uId)
+    {
+        Realm realm;
+        realm = Realm.getDefaultInstance();
+        realm.init(context);
+        EntityUserProfile profile = realm.where(EntityUserProfile.class).findFirst();
+        VolleyHelper volleyHelper = new VolleyHelper(context,context.getResources().getString(R.string.url));
+        volleyHelper.postHeader("users/" + profile.getuID() + "/subscribers/" + uId, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("follow",response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("follow",VolleyHelper.checkErrorCode(error)+"");
+
+            }
+        });
+
+    }
+
+    private void unFollow(int uId)
+    {
+        Realm realm;
+        realm = Realm.getDefaultInstance();
+        realm.init(context);
+        EntityUserProfile profile = realm.where(EntityUserProfile.class).findFirst();
+        VolleyHelper volleyHelper = new VolleyHelper(context,context.getResources().getString(R.string.url));
+        volleyHelper.delete("users/" + profile.getuID() + "/subscribers/" + uId, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("follow",response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("follow",VolleyHelper.checkErrorCode(error)+"");
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/joker/hoclazada/Fragment/FollowerFragment.java
+++ app/src/main/java/com/joker/hoclazada/Fragment/FollowerFragment.java
@@ -3,17 +3,36 @@ package com.joker.hoclazada.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.joker.hoclazada.R;
+import com.joker.hoclazada.Ultil.VolleyHelper;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import Entity.EntityFollow;
+import Entity.EntityUserProfile;
+import adapter.AdapterFollowerList;
+import io.realm.Realm;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FollowerFragment extends Fragment {
-
+    private ListView lvFollowerList;
+    ArrayList<EntityFollow> items;
+    AdapterFollowerList adapterFollowerList;
+    VolleyHelper volleyHelper;
 
     public FollowerFragment() {
         // Required empty public constructor
@@ -23,8 +42,51 @@ public class FollowerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_follower, container, false);
+        View view = inflater.inflate(R.layout.fragment_follower, container, false);
+        lvFollowerList = (ListView) view.findViewById(R.id.lvFollowerList);
+        getFollowerList();
+        return view;
+    }
+
+    private void getFollowerList() {
+        Realm realm;
+        realm = Realm.getDefaultInstance();
+        realm.init(getActivity());
+        EntityUserProfile profile = realm.where(EntityUserProfile.class).findFirst();
+        items = new ArrayList<>();
+        volleyHelper = new VolleyHelper(getActivity(),getResources().getString(R.string.url));
+        volleyHelper.get("users/"+ profile.getuID()+ "/followers", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("data");
+                    Log.d("getFollower",jsonArray.get(0).toString());
+                    for (int i =0; i< jsonArray.length();i++)
+                    {
+                        JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+                        EntityFollow item = new EntityFollow();
+                        item.setId(jsonObject.getString("id"));
+                        item.setUsername(jsonObject.getString("username"));
+                        item.setAvatar(jsonObject.getString("avatar"));
+                        item.setFull_name(jsonObject.getString("full_name"));
+                        item.setIs_followed(jsonObject.getBoolean("is_followed"));
+                        items.add(item);
+                        adapterFollowerList.notifyDataSetChanged();
+                    }
+                    Log.d("result",items.size()+"");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("findUser", VolleyHelper.checkErrorCode(error)+"");
+            }
+        });
+        adapterFollowerList = new AdapterFollowerList(getActivity(),R.layout.custom_follow_list,items);
+        lvFollowerList.setAdapter(adapterFollowerList);
     }
 
 }
+
